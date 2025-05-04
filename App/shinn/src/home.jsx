5d600870ca20ffeb751bf039e38cc7100fb28fdf
unknown <happinessxd743@gmail.com>
2025-05-04 16:00:35 +0545

    react

--- App/shinn/src/home.jsx
+++
@@ -1,4 +0,0 @@
-const Home=()=>{
-    return <h1 style={{color:"azure"}}>Home Pagee hehe</h1>;
-}
-export default Home;
--- App/shinn/src/message.js
+++
@@ -1,4 +0,0 @@
-const MessageBar=({message})=>{
-    return <h1 style={{color:"azure"}}>message</h1>;
-}
-export default MessageBar;
